6fe959330cdd143f8ef576778e745b550313824e
Kevin-mosca <Kevinmoscaa@gmail.com>
2021-04-26 17:07:51 -0700

    first commit

---
+++ videos/script.js
@@ -0,0 +1,14 @@
+function addShadow(element){
+    element.classList.add("shadow");
+}
+
+function removeShadow(element){
+    element.classList.remove("shadow");
+}
+function play(vinylvid){
+    element.classList.add("shadow");
+}
+
+function pause(vinylvid){
+    element.classList.remove("shadow");
+}
